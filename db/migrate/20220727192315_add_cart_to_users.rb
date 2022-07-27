3cabf5af3f9f5792e95b1ecbbd2cac89c69446e6
Henrique Santana <henrique.santana@injunior.com.br>
2022-07-27 16:47:14 -0300

    feat: add chave estrangeira em user.

---
+++ db/migrate/20220727192315_add_cart_to_users.rb
@@ -0,0 +1,5 @@
+class AddCartToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :users, :cart, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_27_043430) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_27_192315) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_27_043430) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "cart_id", null: false
+    t.index ["cart_id"], name: "index_users_on_cart_id"
   end
 
+  add_foreign_key "users", "carts"
 end
