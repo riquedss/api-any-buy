e2d688172e70d797391a9a48e6a6ac1e2ef569a9
Henrique Santana <henrique.santana@injunior.com.br>
2022-07-27 18:15:51 -0300

    chore: migrado a tabela de storage.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_27_201333) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_27_210941) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_27_201333) do
     t.bigint "product_id", null: false
     t.index ["product_id"], name: "index_purchases_on_product_id"
     t.index ["user_id"], name: "index_purchases_on_user_id"
+  end
+
+  create_table "storages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
