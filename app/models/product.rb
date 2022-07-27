84051a425b015e932f0c2b3c608946102e6ea260
Henrique Santana <henrique.santana@injunior.com.br>
2022-07-27 17:15:34 -0300

    feat: add relacionamento n para n, entre produtos e usuários, por meio de compras.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+    has_many :Purchases
+    has_many :users, through: :purchases
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
     belongs_to :cart
+    has_many :Purchases
+    has_many :products, through: :purchases
 end
