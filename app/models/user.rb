b195357738327e095a896d8186127b2feff610db
Henrique Santana <henrique.santana@injunior.com.br>
2022-07-28 20:34:11 -0300

    feat: add criptografia de senha e validação dos tipos de usuários (user e adm).

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
     has_many :products, through: :purchases
     has_many :favorites, dependent: :destroy
     has_many :product, through: :favorites
+
+    has_secure_password
+
+    enum kind: { "user": 0, "adm": 1 }
 end
