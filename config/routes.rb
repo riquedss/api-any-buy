60ef8350a7a7918475022c5e568f14cae4b3b207
Henrique Santana <henrique.santana@injunior.com.br>
2022-07-28 14:08:28 -0300

    feat: add controller de auth.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'auth/login'
+  get 'auth/signup'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
