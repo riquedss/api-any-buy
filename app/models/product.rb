61a999225dd872a5c0a1faa387d4ce94d9045fcc
Henrique Santana <henrique.santana@injunior.com.br>
2022-07-28 02:05:43 -0300

    feat: add relacionamento n para n, usuário para produto por meio de favoritos.

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,6 @@ class Product < ApplicationRecord
     has_many :users, through: :purchases
     has_many :storages
     has_many :carts, through: :storages
+    has_many :favorites, dependent: :destroy
+    has_many :user, through: :favorites
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     belongs_to :cart
     has_many :Purchases
     has_many :products, through: :purchases
+    has_many :favorites, dependent: :destroy
+    has_many :product, through: :favorites
 end
