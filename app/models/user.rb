8fee1702355d7efbe573e947b2e080e4d63ce04e
Henrique Santana <henrique.santana@injunior.com.br>
2022-07-28 20:53:15 -0300

    fix: correção no campo id_cart em usuário, sendo, agora, opcional.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    belongs_to :cart
+    belongs_to :cart, optional: true
     has_many :Purchases
     has_many :products, through: :purchases
     has_many :favorites, dependent: :destroy
