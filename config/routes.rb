b9867e84890260c970ba46a7c619575df7887671
Henrique Santana <henrique.santana@injunior.com.br>
2022-07-29 11:57:48 -0300

    feat: rota para buscar produto pelo tipo.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   post 'login', to: 'auth#login'
   post 'signup', to: 'auth#signup'
 
+  get 'products/konsol-game', to: 'products#index_konsol_game'
+  get 'products/laptop', to: 'products#index_laptop'
+  get 'products/smartphone', to: 'products#index_smartphone'
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
