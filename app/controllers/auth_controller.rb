6f683994b91c665151a1c6e8f6f236981a94c02a
Henrique Santana <henrique.santana@injunior.com.br>
2022-07-28 23:16:20 -0300

    feat: add função de login.

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,5 +1,13 @@
 class AuthController < ApplicationController
   def login
+    @user = User.find_by(email: set_user[:email])
+    byebug
+    if @user && @user.authenticate(set_user[:password])
+      token = JsonWebToken::Base.encode(User_id: @user.id)
+      render json: { token: token }, status: :ok
+    else
+      render json: { message: "Password or E-mail Incorrect" }, status: :unauthorized
+    end
   end
 
   def signup
@@ -14,5 +22,9 @@ class AuthController < ApplicationController
   private
   def params_user
     params.require(:user).permit(:name, :last_name, :phone, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    params.require(:user).permit(:email, :password)
   end
 end
