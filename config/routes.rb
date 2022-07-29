5bfb2bb157baf3be8c18f4b1e1640dfb03dbe50b
Henrique Santana <henrique.santana@injunior.com.br>
2022-07-29 10:32:34 -0300

    feat: rotas para o crud de produtos.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   post 'login', to: 'auth#login'
   post 'signup', to: 'auth#signup'
+
+  resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
