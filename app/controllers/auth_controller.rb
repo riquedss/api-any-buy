831938698e35a8a21bc3086cb0306155aca70b64
Henrique Santana <henrique.santana@injunior.com.br>
2022-07-28 22:05:38 -0300

    feat: add função de signup.

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,5 +3,16 @@ class AuthController < ApplicationController
   end
 
   def signup
+    @user = User.new(params_user)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def params_user
+    params.require(:user).permit(:name, :last_name, :phone, :email, :password, :password_confirmation)
   end
 end
