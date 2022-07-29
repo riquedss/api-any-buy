706cfcd0e31a483a232a85d2bcd851151a1cc7f5
Henrique Santana <henrique.santana@injunior.com.br>
2022-07-29 10:31:41 -0300

    feat: enum para os tipos de produto.

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,6 @@ class Product < ApplicationRecord
     has_many :carts, through: :storages
     has_many :favorites, dependent: :destroy
     has_many :user, through: :favorites
+
+    enum kind: { "konsol_game": 0, "laptop": 1, "smartphone": 2 }
 end
