8498336c0e507bff7376d247bb5f353f7e4951b4
Henrique Santana <henrique.santana@injunior.com.br>
2022-07-30 00:03:46 -0300

    feat: rotas do crud de usuário e para transformar user em adm.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   post 'login', to: 'auth#login'
   post 'signup', to: 'auth#signup'
 
+  put 'users/adm/:id', to: 'users#update_adm'
+  resources :users
+
   get 'products/konsol-game', to: 'products#index_konsol_game'
   get 'products/laptop', to: 'products#index_laptop'
   get 'products/smartphone', to: 'products#index_smartphone'
