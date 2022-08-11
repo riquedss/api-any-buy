4f964881cea2abfe24021e327b287105b9693d54
Henrique Santana <henrique.santana@injunior.com.br>
2022-08-11 20:35:19 -0300

    feat(cart): add rotas

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   put 'users/adm/:id', to: 'users#update_adm'
   resources :users
   resources :products
+  resources :carts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
