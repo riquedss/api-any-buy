d9c4e304ac3b7cd1761153c7f12ad0cf740ed14d
Henrique Santana <henrique.santana@injunior.com.br>
2022-08-19 23:19:30 -0300

    feat: add autenticação e função de add produto ao carrinho

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class CartsController < ApplicationController
+  before_action only: %i[update] do @user = verify_authenticated('adm') end
+  before_action except: %i[update] do @user = verify_authenticated end
   before_action :set_cart, only: %i[show update]
 
   def index
@@ -18,6 +20,16 @@ class CartsController < ApplicationController
     else
       render(json: @cart.errors, status: :unprocessable_entity)
     end
+  end
+
+  def add_product_in_cart
+    @product = Product.find(params[:id])
+    render(json: @product.errors) unless @product
+
+    @cart_user = @user.cart
+
+    @storage = Storage.new({ product_id: params[:id], cart_id: @cart_user.id })
+    render(json: @cart_user.storages, status: :created) if @storage.save
   end
 
   private
